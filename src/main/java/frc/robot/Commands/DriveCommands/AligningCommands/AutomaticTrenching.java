b6d9e5252425842ff82b8791fa95151529fb5b6a
TheControlsExpert <roboticsfanatic4338@gmail.com>
2026-02-10 17:37:51 +0200

    added method to find closest pose for going under trench

--- src/main/java/frc/robot/Commands/DriveCommands/AligningCommands/AutomaticTrenching.java
+++ src/main/java/frc/robot/Commands/DriveCommands/AligningCommands/AutomaticTrenching.java
@@ -9,7 +9,10 @@ import com.pathplanner.lib.path.Waypoint;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -22,21 +25,64 @@ public class AutomaticTrenching extends SequentialCommandGroup {
 
     public AutomaticTrenching(Drive swervy) {
         this.swerve = swervy;
-
-        constraints = new PathConstraints(swervy.getMaxLinearSpeedMetersPerSec(), swervy.getMaxLinearSpeedMetersPerSec() / 2, swervy.getMaxAngularSpeedRadPerSec(), swervy.getMaxAngularSpeedRadPerSec() / 2);
-        PathPlannerPath trenchPath = PathPlannerPath.fromPathFile("Automatic Pathing Test");
-
-        List<Waypoint> waypoints = PathPlannerPath.waypointsFromPoses(
-        new Pose2d(swervy.get, 1.0, Rotation2d.fromDegrees(0)),
-        new Pose2d(3.0, 1.0, Rotation2d.fromDegrees(0)),
-        new Pose2d(5.0, 3.0, Rotation2d.fromDegrees(90))
-);
-
+    
+        addRequirements(swervy);
         
-        , new InstantCommand(swervy::resetGyro, swervy));
-
         
     }
+
+
+
+
+
+    public Pose2d getClosestPathingPose() {
+        Pose2d currentPose = swerve.getEstimatedPosition();
+
+        Translation2d pathStart_blue_bottom = new Translation2d(5.807, 0.685);
+
+        if (DriverStation.getAlliance().equals(Alliance.Blue)) {
+                double distance_bottom = currentPose.getTranslation().getDistance(pathStart_blue_bottom);
+                Translation2d pathStart_blue_top = FlipVertically_bottom_to_top(pathStart_blue_bottom);
+                double distance_top = currentPose.getTranslation().getDistance(pathStart_blue_top);
+    
+                if (distance_bottom < distance_top) {
+                    return new Pose2d(pathStart_blue_bottom, new Rotation2d(Math.PI));
+                } else {
+                    return new Pose2d(pathStart_blue_top, new Rotation2d(Math.PI));
+                }
+
+            // 
+        }
+
+        else {
+            //assume red
+            Translation2d pathStart_red_bottom = FlipHorizontally_BtoR(pathStart_blue_bottom);
+            Translation2d pathStart_red_top = FlipVertically_bottom_to_top(pathStart_red_bottom);
+
+
+            double distance_bottom = currentPose.getTranslation().getDistance(pathStart_red_bottom);
+            double distance_top = currentPose.getTranslation().getDistance(pathStart_red_top);
+
+            if (distance_bottom < distance_top) {
+                return new Pose2d(pathStart_red_bottom, new Rotation2d());
+            } else {
+                return new Pose2d(pathStart_red_top, new Rotation2d());
+            }
+        }
+
+    } 
+
+
+
+    //flips translation2d from blue side to red side
+    public Translation2d FlipHorizontally_BtoR(Translation2d point) {
+        return new Translation2d( 2* (8.219694 - point.getX()) + point.getX(), point.getY()); 
+    }
+
+    //flips translation2d from bottom of blue to top of blue
+    public Translation2d FlipVertically_bottom_to_top(Translation2d point) {
+        return new Translation2d( point.getX(), 2* (4.021328 - point.getY()) + point.getY()); 
+     }
 
 
     
