18d7a68ccb345db65ff285462e18301c531f96a9
NunmanGrayson <adamigrayson@gmail.com>
2026-02-26 16:11:31 +0200

    added climb enums file

---
+++ src/main/java/frc/robot/climbEnums.java
@@ -0,0 +1,28 @@
+package frc.robot;
+
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public enum AutoEnums {
+    
+    NO_LOADER,
+    ONE_LOADER,
+    TWO_LOADERS;
+
+    
+}
+
+public enum climbEnums {
+
+    YES_CLIMB,
+    NO_CLIMB;
+
+
+    
+    
+
+    
+    
+    
+}
+
