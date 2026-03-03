5662ac702dc26e4b1815a2bab747f2adab258961
NunmanGrayson <adamigrayson@gmail.com>
2026-03-03 14:19:38 +0200

    tried to fix the climbing buttton bindings for tele climbing. Still no climbing up so our bot just stays on the tower.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -259,8 +259,8 @@ public class RobotContainer {
                 controller));
         controller.leftBumper().whileTrue(new IntakeCommand(intake));
 
-        //controller.button(8).whileTrue(autoClimbing.getClimbingCommand()); //climbing commands
-       // controller.button(7).onTrue(new ClimbDown(climb)); 
+        controller.button(8).whileTrue(Commands.defer(() -> autoClimbing.getClimbingCommand(), Set.of(climb, drive))); //climbing commands
+        controller.button(7).onTrue(Commands.defer(() -> new ClimbDown(climb, drive), Set.of(climb))); 
          
 
          controller.rightTrigger().whileTrue(new InstantCommand(() -> {intake.setIntakeDutyCycle(0.3);}, intake)
