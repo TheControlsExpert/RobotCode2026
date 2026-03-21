76e8505efcbaf6a827643a8211de2450da133e00
NunmanGrayson <adamigrayson@gmail.com>
2026-03-21 22:50:50 +0200

    added isFinished() and end()

--- src/main/java/frc/robot/Commands/ShootingCommands/shootingPathsAuto.java
+++ src/main/java/frc/robot/Commands/ShootingCommands/shootingPathsAuto.java
@@ -4,6 +4,7 @@ import com.pathplanner.lib.path.PathPlannerPath;
 
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Subsystems.Drive.Drive;
 import frc.robot.Subsystems.Indexer.Indexer;
@@ -17,10 +18,11 @@ public class shootingPathsAuto extends Command{
 
     Translation2d initialPos;
     Translation2d finalPos;
-
     PathPlannerPath nextPath;
-
     ChassisSpeeds MOVE;
+    Timer timer = new Timer();
+    double timeout = 1;
+
 
     public shootingPathsAuto(Shooter shooter, Drive drive, Indexer indexer, PathPlannerPath nextPath) {
         this.drive = drive;
@@ -29,22 +31,32 @@ public class shootingPathsAuto extends Command{
         this.nextPath = nextPath;
         addRequirements(shooter, drive, indexer);
     }
+    
+
     public void initialize() {
+        timer.restart();
+
         initialPos = drive.getEstimatedPosition().getTranslation();
         finalPos = nextPath.getStartingHolonomicPose().get().getTranslation();
         
         Translation2d distanceVector = finalPos.minus(initialPos);
-        double rawSpeed = finalPos.getDistance(initialPos) / 1;
+        double rawSpeed = finalPos.getDistance(initialPos) / timeout;
 
         Translation2d linearVelocity = distanceVector.times(rawSpeed/distanceVector.getNorm());
 
         MOVE = new ChassisSpeeds(
-                        linearVelocity.getX(),
-                        linearVelocity.getY(),
-                        0);
+                linearVelocity.getX(),
+                linearVelocity.getY(), 0);
+    }
 
 
 
+    public boolean isFinished () {
+        return timer.hasElapsed(timeout) && drive.getEstimatedPosition().getTranslation().getDistance(finalPos) < 0.01;
+    }
+
+    public void end (boolean interrupted) {
+        //set the chassis speed to 0
     }
 
 }
