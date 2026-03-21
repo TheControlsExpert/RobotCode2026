ae43bc58d9af50e1eda0ea84a097086df48187c0
NunmanGrayson <adamigrayson@gmail.com>
2026-03-21 21:46:53 +0200

    quick stop

--- src/main/java/frc/robot/Commands/ShootingCommands/shootingPathsAuto.java
+++ src/main/java/frc/robot/Commands/ShootingCommands/shootingPathsAuto.java
@@ -1,22 +1,35 @@
 package frc.robot.Commands.ShootingCommands;
 
+import com.pathplanner.lib.path.PathPlannerPath;
+
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Subsystems.Drive.Drive;
 import frc.robot.Subsystems.Indexer.Indexer;
 import frc.robot.Subsystems.Shooter.Shooter;
 
 public class shootingPathsAuto extends Command{
+
     Shooter shooter;
     Drive drive;
     Indexer indexer;
-    public shootingPathsAuto(Shooter shooter, Drive drive, Indexer indexer) {
-        this.drive =drive;
+
+    Translation2d initialPos;
+    Translation2d finalPos;
+
+    PathPlannerPath nextPath;
+
+    public shootingPathsAuto(Shooter shooter, Drive drive, Indexer indexer, PathPlannerPath nextPath) {
+        this.drive = drive;
         this.shooter = shooter;
         this.indexer = indexer;
+        this.nextPath = nextPath;
         addRequirements(shooter, drive, indexer);
     }
     public void initialize() {
+        initialPos = drive.getEstimatedPosition().getTranslation();
         
+
     }
 
 }
