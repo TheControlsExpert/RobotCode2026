6ff1c06e91dc4c9b5bd2985e64801f1bbd8edbee
yotam <yotam.karni2011@gmail.com>
2026-03-22 22:12:19 +0200

    finally understood the physics

--- src/main/java/frc/robot/Commands/ShootingCommands/shootingPathsAuto.java
+++ src/main/java/frc/robot/Commands/ShootingCommands/shootingPathsAuto.java
@@ -1,22 +1,37 @@
 package frc.robot.Commands.ShootingCommands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
 import frc.robot.Subsystems.Drive.Drive;
 import frc.robot.Subsystems.Indexer.Indexer;
 import frc.robot.Subsystems.Shooter.Shooter;
+
+import com.pathplanner.lib.path.PathPlannerPath;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
 
 public class shootingPathsAuto extends Command{
     Shooter shooter;
     Drive drive;
     Indexer indexer;
-    public shootingPathsAuto(Shooter shooter, Drive drive, Indexer indexer) {
+    PathPlannerPath path;
+    public shootingPathsAuto(Shooter shooter, Drive drive, Indexer indexer,PathPlannerPath path) {
         this.drive =drive;
         this.shooter = shooter;
         this.indexer = indexer;
+        this.path = path;
         addRequirements(shooter, drive, indexer);
     }
     public void initialize() {
-        
+        Translation2d currentPos = drive.getEstimatedPosition().getTranslation();
+        Translation2d wantedPos = path.getStartingHolonomicPose().get().getTranslation();
+        double distance = currentPos.getDistance(wantedPos);
+        double speed = drive.getEstimatedPosition().getTranslation().getDistance(wantedPos) / 3;
+        if(speed>Constants.ShooterConstants.maxMovingSpeed) {
+            speed = Constants.ShooterConstants.maxMovingSpeed;
+        }
+        speed = 
     }
 
 }
