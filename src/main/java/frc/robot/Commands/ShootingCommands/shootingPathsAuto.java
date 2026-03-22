f295a04cc7a4b9e03724944128a3f2d6361f2566
NunmanGrayson <adamigrayson@gmail.com>
2026-03-22 02:11:35 +0200

    done with vecotrs and rotations

--- src/main/java/frc/robot/Commands/ShootingCommands/shootingPathsAuto.java
+++ src/main/java/frc/robot/Commands/ShootingCommands/shootingPathsAuto.java
@@ -2,6 +2,8 @@ package frc.robot.Commands.ShootingCommands;
 
 import com.pathplanner.lib.path.PathPlannerPath;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.Timer;
@@ -16,19 +18,21 @@ public class shootingPathsAuto extends Command{
     Drive drive;
     Indexer indexer;
 
-    Translation2d initialPos;
-    Translation2d finalPos;
+    Pose2d initialPose;
+    Pose2d finalPose;
     PathPlannerPath nextPath;
     ChassisSpeeds MOVE;
     Timer timer = new Timer();
     double timeout = 1;
+    double kP_Rotation;
 
 
-    public shootingPathsAuto(Shooter shooter, Drive drive, Indexer indexer, PathPlannerPath nextPath) {
+    public shootingPathsAuto(Shooter shooter, Drive drive, Indexer indexer, PathPlannerPath nextPath, double kP_Rotation) {
         this.drive = drive;
         this.shooter = shooter;
         this.indexer = indexer;
         this.nextPath = nextPath;
+        this.kP_Rotation = kP_Rotation;
         addRequirements(shooter, drive, indexer);
     }
     
@@ -36,13 +40,18 @@ public class shootingPathsAuto extends Command{
     public void initialize() {
         timer.restart();
 
-        initialPos = drive.getEstimatedPosition().getTranslation();
-        finalPos = nextPath.getStartingHolonomicPose().get().getTranslation();
+        initialPose = drive.getEstimatedPosition();
+        finalPose = nextPath.getStartingHolonomicPose().get();
         
-        Translation2d distanceVector = finalPos.minus(initialPos);
-        double rawSpeed = finalPos.getDistance(initialPos) / timeout;
+        //all the velocity stuff
+        Translation2d distanceVector = finalPose.getTranslation().minus(initialPose.getTranslation()); //distance vector
+        double rawSpeed = finalPose.getTranslation().getDistance(initialPose.getTranslation()) / timeout; //scalar velocity
+        Translation2d linearVelocity = distanceVector.times(rawSpeed/distanceVector.getNorm()); //now the velocity is vector
 
-        Translation2d linearVelocity = distanceVector.times(rawSpeed/distanceVector.getNorm());
+        double targetRotation = finalPose.getTranslation().minus(initialPose.getTranslation()).getAngle().getRadians();
+        double deltaRotation = targetRotation - drive.getEstimatedPosition().getRotation().getRadians();
+        deltaRotation = MathUtil.angleModulus(deltaRotation);
+        double Ilan = deltaRotation * kP_Rotation;
 
         MOVE = new ChassisSpeeds(
                 linearVelocity.getX(),
